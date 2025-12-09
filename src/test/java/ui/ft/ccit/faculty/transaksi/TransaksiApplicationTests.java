8fab9d564eb1337605bf3e46c01900103ce8332a
lovyoeh <najmirarahmadina@gmail.com>
2025-12-09 22:26:53 +0700

    ntar revisi lagi

--- src/test/java/ui/ft/ccit/faculty/transaksi/TransaksiApplicationTests.java
+++ src/test/java/ui/ft/ccit/faculty/transaksi/TransaksiApplicationTests.java
@@ -2,11 +2,8 @@ package ui.ft.ccit.faculty.transaksi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-
 class TransaksiApplicationTests {
 
 	@Test
