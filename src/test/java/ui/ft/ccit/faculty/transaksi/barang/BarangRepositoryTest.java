1a75ed79e99ffdd2e67cbe23d93eee66ab95fbcc
lovyoeh <najmirarahmadina@gmail.com>
2025-12-09 22:10:35 +0700

    revisi ntar cek lagi

--- src/test/java/ui/ft/ccit/faculty/transaksi/barang/BarangRepositoryTest.java
+++ src/test/java/ui/ft/ccit/faculty/transaksi/barang/BarangRepositoryTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@ActiveProfiles("local")
+@ActiveProfiles("test")
 class BarangRepositoryTest {
 
         @Autowired
